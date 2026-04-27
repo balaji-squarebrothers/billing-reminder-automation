ab6ff0fc98736f8ff08e4e5de0235300277e9e5c
Balaji Selvam <balaji.selvam@squarebrothers.com>
2026-04-27 17:33:49 +0530

    Commit before implementing admin feature

--- billing_reminder/settings.py
+++ billing_reminder/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-print("EMAIL:", os.getenv("EMAIL_HOST_USER"))
-print("PASS:", os.getenv("EMAIL_HOST_PASSWORD"))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
